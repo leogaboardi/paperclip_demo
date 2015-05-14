00892a0567b7b41d433c4064c7ddd33258bde07b
Leonardo Gaboardi <leogaboardi@gmail.com>
2015-05-13 20:46:53 -0500

    v9

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -7,7 +7,10 @@ class Friend < ActiveRecord::Base
   has_attached_file :avatar,
     :styles => { thumb: '100x100>', square: '200x200#', medium: '300x300>' },
     :storage => :s3,
-    :bucket => 'hohohohoho',
-    :s3_credentials => "#{Rails.root}/config/aws.yml",
+    :bucket => ENV['AWS_BUCKET'],
+    :s3_credentials => {
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    },
     :path => "resources/:id/:style/:basename.:extension"
 end
