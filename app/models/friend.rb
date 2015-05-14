5f4000b529b22aae488b9a79d88cc92b68240822
Leonardo Gaboardi <leogaboardi@gmail.com>
2015-05-13 20:27:41 -0500

    v3

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -4,9 +4,15 @@ class Friend < ActiveRecord::Base
     attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
     attachment_size: { less_than: 5.megabytes }
 
-  has_attached_file :avatar, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
-  }
+  has_attached_file
+    :avatar,
+    styles: {
+      thumb: '100x100>',
+      square: '200x200#',
+      medium: '300x300>'
+      },
+    :storage => :s3,
+    :bucket => 'mppbay',
+    :s3_credentials => "#{Rails.root}/config/aws.yml",
+    :path => "resources/:id/:style/:basename.:extension
 end
